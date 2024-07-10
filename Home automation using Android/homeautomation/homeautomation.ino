7efb282ebae6bd4614ed0f1b0de093c9a23c3b8b
maddrills <mathewfvv@gmail.com>
2024-07-10 11:55:30 +0530

    Home Automation code

---
+++ Home automation using Android/homeautomation/homeautomation.ino
@@ -0,0 +1,128 @@
+#include <SoftwareSerial.h>
+#include "DHT.h"
+
+ // Arduino pin connected to relay which connected to fan
+#define DHTPIN 12           // Arduino pin connected to relay which connected to DHT sensor
+#define DHTTYPE DHT11
+
+//below is for light sensor
+#define LIGHT 7 // define pint  for sensor
+#define RELAY 2 // define pin 9 as for relay
+
+const int TEMP_THRESHOLD_UPPER = 31; // upper threshold of temperature, change to your desire value
+const int TEMP_THRESHOLD_LOWER = 30; // lower threshold of temperature, change to your desire value
+
+const int RELAY_FAN_PIN = 3;  // Arduino pin connected to relay which connected to fan
+
+SoftwareSerial BT(10, 11); //TX, RX  pins of arduino respetively
+
+DHT dht(DHTPIN, DHTTYPE);
+
+float temperature;    // temperature in Celsius
+
+String command;
+void setup()
+{
+  BT.begin(9600);
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  dht.begin();
+
+  //light
+  pinMode(LIGHT, INPUT_PULLUP);// define pin as Input  sensor
+  pinMode(RELAY, OUTPUT);// define pin as OUTPUT for relay
+}
+
+void loop() 
+{
+  while (BT.available())
+  {  
+    //Check if there is an available byte to read
+    delay(10); //Delay added to make thing stable
+    char c = BT.read(); //Conduct a serial read
+    command += c; //build the string.
+  } 
+  if (command.length() > 0) 
+  {
+    Serial.println(command);
+    if(command == "light on") //this command will be given as an input to switch on light1
+    {
+      digitalWrite(2, HIGH);
+    }
+    else if(command == "light off") //this command will be given as an input to switch off light1 simillarly other commands work
+    {
+      digitalWrite(2, LOW);
+    }
+
+    else if(command == "lamp on") //this command will be given as an input to switch off light1 simillarly other commands work
+    {
+      while(true)
+      {
+        // wait a few seconds between measurements.
+        delay(120);
+      
+        temperature = dht.readTemperature(); // read temperature in Celsius
+        
+        if (isnan(temperature))
+        {
+          Serial.println("Failed to read from DHT sensor!");
+        }
+        else 
+        {
+          if(temperature > TEMP_THRESHOLD_UPPER)
+          {
+            Serial.println("The fan is turned on");
+            digitalWrite(3, HIGH); // turn on
+          } 
+          else if(temperature < TEMP_THRESHOLD_LOWER)
+          {
+            Serial.println("The fan is turned off");
+            digitalWrite(3, LOW); // turn on
+          }
+        }
+        // Light LDR Sensor Code by Robojax.com 20180210
+        int L =digitalRead(LIGHT);// read the sensor 
+        
+        if(L == 1)
+        {
+          Serial.println(" light is ON");
+          digitalWrite(RELAY,HIGH);// turn the relay ON
+        }
+        else
+        {
+          Serial.println("  === light is OFF");
+          digitalWrite(RELAY,LOW);// turn the relay OFF
+        }
+        delay(500);
+        /*if(command == "lamp off") //this command will be given as an input to switch off light1 simillarly other commands work
+        {
+          break;
+        }*/
+
+      }
+              {
+          digitalWrite(2, LOW);
+        }
+    }//////////////
+    else if (command == "fan on")
+    {
+      digitalWrite (3, HIGH);
+    }
+    else if (command == "fan of")
+    {
+      digitalWrite (3, LOW);
+    }
+    else if (command == "all on") //using this command you can switch on all devices
+    {
+      digitalWrite (2, HIGH);
+      digitalWrite (3, HIGH);
+    }
+    else if (command == "off")//using this command you can switch off all devices
+    {
+      digitalWrite (2, LOW);
+      digitalWrite (3, LOW);
+    }
+    command="";
+  }
+} 
